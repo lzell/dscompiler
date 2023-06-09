4a2c4e3316a540bd5dc9ec5b75d2b484ced1c1c0
Lou Zell <lzell11@gmail.com>
2023-06-09 13:55:39 -0700

    Pass messages from UI to plugin sandbox

--- figma_plugin/src/ui.ts
+++ figma_plugin/src/ui.ts
@@ -1,3 +1,12 @@
 import { saveAs } from 'file-saver'
 const blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"})
 saveAs(blob, "hello world.txt")
+
+const closeButton = document.getElementById('close-button')
+if (closeButton) {
+  closeButton.onclick = () => {
+    parent.postMessage({ pluginMessage: { type: 'close-button-action' } }, '*')
+  }
+} else {
+  console.assert(false, "Expected the UI to contain a tag with id 'close-button'")
+}
